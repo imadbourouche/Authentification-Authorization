72171abc6d7d6b7a14fe52d0b6d2485517fed694
Imad Bourouche <ji_bourouche@esi.dz>
2022-12-12 09:52:02 +0100

    Delete basic_auth directory

--- basic_auth/index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const app = require('./app')
-
-const port = 8888
-const server = http.createServer(app)
-
-server.listen(port, ()=>{
-    console.log(`server running on http://127.0.0.1:${port}`)
-})
